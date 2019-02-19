b9791b48dfe783177572858d4322e1eaef81b574
Shankar <shankar21mail@gmail.com>
2019-02-19 18:06:51 +0530

    Fix: Update header component and its style

--- src/v1.1/components/main_nav/index.js
+++ src/v1.1/components/main_nav/index.js
@@ -1,57 +1,62 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-static'
 import classNames from 'classnames'
 //
+import Button from '../button/index'
 import { genRandomKey } from '../../utils'
 //
 import content from './content.json'
 import './style.sass'
 
-class NavItem extends Component {
-  render() {
-    const { data, currentLocationPath, isButton } = this.props
+const NavItem = props => {
+  const { data, currentLocationPath, isButton } = props
 
-    return (
-      <div className={classNames(
-          {
-            mainNav__item: !isButton,
-            'mainNav__item-btn': isButton,
-            active: (data.link === currentLocationPath),
-          }
-        )}
-      >
-        <Link to={data.link.trim() || '#'}>{data.name}</Link>
-      </div>
-    )
-  }
+  const linkEle = <Link to={data.link.trim() || '#'}>{data.name}</Link>
+
+  return (
+    <div className={classNames(
+        {
+          mainNav__item: !isButton,
+          'mainNav__item-btn': isButton,
+          active: (data.link === currentLocationPath),
+        }
+      )}
+    >
+      {
+        isButton ? <Button className="primary">{linkEle}</Button> : linkEle
+      }
+    </div>
+  )
 }
 
-export default class MainNav extends Component {
-  items = content.navItems
-  itemButton = content.navButton
+const MainNav = props => {
+  const { location } = props
+  const { pathname } = location
+  const { navItems: items, navButton: itemButton } = content
 
-  render() {
-    const { location } = this.props
-    const { pathname } = location
+  const isLightTheme = (pathname === '/get-involved/')
 
-    const isLightTheme = (pathname === '/get-involved/')
-
-    return (
-      <div className={classNames('mainNav', {
-        light: isLightTheme,
-      })}>
-        <div className="mainNav__wrap">
-          <div className="mainNav__items">
-            {
-              this.items.map(data =>
-                <NavItem key={genRandomKey()} data={data} currentLocationPath={pathname} />)
-            }
-          </div>
-          <div className="mainNav__btnItems">
-            <NavItem data={this.itemButton} currentLocationPath={pathname} isButton />
-          </div>
+  return (
+    <div className={classNames('mainNav', {
+      light: isLightTheme,
+    })}>
+      <div className="mainNav__wrap">
+        <div className="mainNav__items">
+          {
+            (items && items.length > 0) ? (items.map(data => (
+              <NavItem
+                key={genRandomKey()}
+                data={data}
+                currentLocationPath={pathname}
+              />))) : null
+          }
+        </div>
+        <div className="mainNav__btnItems">
+          <NavItem data={itemButton} currentLocationPath={pathname} isButton />
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default MainNav
