3e1edca00ce46d0c785f8a7140c2fc4c4d766aef
Shankar <shankar21mail@gmail.com>
2019-03-06 17:47:14 +0530

    Fix: Bring navItem upfront

--- src/v1.1/components/GlobalHeader/MainNav/MainNav.js
+++ src/v1.1/components/GlobalHeader/MainNav/MainNav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import classNames from 'classnames'
 
-import NavItem from './NavItem'
+import NavItem from '../NavItem'
 import { genRandomKey } from '~src/utils'
 
 import './mainNav.sass'
