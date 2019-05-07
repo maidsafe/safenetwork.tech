249fd4e1c685bb8e811da6d32dda9c6e16ac7074
Shankar <shankar21mail@gmail.com>
2019-05-07 11:51:43 +0530

    Feat: Cookie and Privacy pages implemented

---
+++ src/v1.1/pages/Cookies/Topic/Topic.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import classNames from 'classnames'
+
+import { prefixClassName, genRandomKey, parseMDText } from 'src/utils'
+
+import './topic.sass'
+
+const baseClassName = 'CookieTopic'
+const cn = prefixClassName(baseClassName)
+
+const Topic = props => {
+  const { content, numberedPara = false } = props
+  const { title, index, para, subTopic = null } = content
+
+  let subTopicEle = []
+
+  const rowClassName = classNames(cn('row'), {
+    numbered: numberedPara
+  })
+
+  const getPara = p => (
+    <div className={cn('para')}><p>{parseMDText(p, true)}</p></div>
+  )
+
+  if (subTopic) {
+    if (subTopic.title) {
+      subTopicEle.push(<div key={genRandomKey()} className={rowClassName}>
+        <div className={cn('index')}><h3>{subTopic.index}</h3></div>
+        <div className={cn('subTitle')}><h3>{subTopic.title}</h3></div>
+      </div>)
+    }
+    if (subTopic.para) {
+      subTopicEle = subTopicEle.concat(
+        subTopic.para.map(p => (
+          <div key={genRandomKey()} className={cn('row')}>{getPara(p)}</div>
+        ))
+      )
+    }
+  }
+
+  return (
+    <div className={baseClassName}>
+      <div className={cn('wrap')}>
+        <div className={cn('row')}>
+          <div className={cn('index')}><h2>{index}</h2></div>
+          <div className={cn('title')}><h2>{title}</h2></div>
+        </div>
+        {
+          para.map((p, i) => (
+            <div key={genRandomKey()} className={rowClassName}>
+              {numberedPara ? <div className={cn('index')}><h3>{`${index}${i+1}`}</h3></div> : null}
+              {getPara(p)}
+            </div>
+          ))
+        }
+        {subTopicEle}
+      </div>
+    </div>
+  )
+}
+
+export default Topic
---
+++ src/v1.1/pages/Privacy/content.js
@@ -0,0 +1,31 @@
+export default {
+  title: 'Privacy Policy',
+  topics: [
+    {
+      title: 'Data controller: MaidSafe.Net Limited',
+      index: '1.',
+      para: [
+        'We take our duty to process your personal data very seriously. This policy explains how we collect, manage, use and protect your personal data.',
+        'We may change this policy from time to time to reflect the latest view of what we do with your information. Please check back frequently to see if we have made any changes.'
+      ],
+    }, {
+      title: 'What does this policy cover?',
+      index: '2.',
+      para: [
+        'This privacy policy applies only to your use of our site. Our site may contain links to other websites. Please note that we have no control over how your data is collected, stored, or used by other websites and we advise you to check the privacy policies of any such websites before providing any data to them.'
+      ]
+    }, {
+      title: 'What is personal data?',
+      index: '3.',
+      para: [
+        'Personal data means any and all data that relates to an identifiable person who can be directly or indirectly identified from that data. In this case, it means personal data that you give to us via Our Site. This definition shall, where applicable, incorporate the definitions provided in the EU Regulation 2016/679 – the General Data Protection Regulation (“GDPR”).'
+      ]
+    }, {
+      title: 'What information does the organisation collect?',
+      index: '4.',
+      para: [
+        'We do not place cookies on your computer or device however, the organisation collects information about you as follows:'
+      ]
+    }
+  ]
+}
