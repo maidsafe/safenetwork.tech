ddae7234269fadc017f790ccd1012f2b68b2f12a
Shankar <shankar21mail@gmail.com>
2019-05-07 12:29:49 +0530

    Feat: About MaidSafe and 404 pages implemented

---
+++ src/v1.1/pages/About/content.js
@@ -0,0 +1,14 @@
+export default {
+  desc: 'The company that originally envisaged the SAFE Network, MaidSafe are driven by one overriding goal: to provide privacy, security and freedom for everyone on the planet.',
+  CTA: {
+    name: 'Contact MaidSafe',
+    url: 'https://maidsafe.net/'
+  },
+  para: [
+    'In 2006, MaidSafe began work on a decentralised internet that is private, secure and free for everybody. Since those early days, the team, led by founder David Irvine, has been focused solely on building the SAFE Network to solve the problems of an increasingly centralised internet that is facing more issues with each day that passes.',
+    'MaidSafe are based in Ayr, Scotland but the team is distributed all across the globe. It has has drawn talents from many different cultures and countries, reflecting the users that they serve. Comprised of thinkers, inventors, tinkerers, PHD’s, engineers and designers, they are building software that is open source and free to use and share in a single mission: a desire to provide security and privacy for everyone.',
+    'MaidSafe are guided and influenced by the SAFE Network Community who have helped to fund the work and guide its decisions through discussions across community-run platforms.',
+    'The SAFE Network is currently in Alpha Release. Upon full release, MaidSafe will be only one of many thousands of developers making a living by carrying out development on the Network as a result of the built-in incentivisation scheme.',
+    'To contact the team directly, visit the company website at maidsafe.net for further details.'
+  ]
+}
