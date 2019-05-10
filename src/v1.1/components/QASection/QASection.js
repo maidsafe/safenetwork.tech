d2465a8cedef932e4417bcb827a035bbf88fa7e1
Shankar <shankar21mail@gmail.com>
2019-05-10 08:08:27 +0530

    Fundamental page implemented (#284)
    * Feat: Fundamental page implemented
    
    * Fix: Replace fundamental accordion with roadmap accordion component
    
    * Fix: Styling for Safecoin section and Accordion section
    
    * Fix: Accordion minor style fix

--- src/v1.1/components/QASection/QASection.js
+++ src/v1.1/components/QASection/QASection.js
@@ -7,15 +7,25 @@ import { genRandomKey } from 'src/utils'
 import './qaSection.sass'
 
 const QASection = props => {
-  const { content } = props
-  const { title, items } = content
+  const LIST_TYPE = {
+    NUM: 'NUM'
+  }
+
+  const { content, listType = null } = props
+  const { title, items = [] } = content
 
   return (
     <div className="qaSection">
       <div className="qaSection__wrap">
         <div className="qaSection__title"><h3>{title}</h3></div>
         {
-          items.map(item => <QAItem key={genRandomKey()} content={item} />)
+          items.map((item, index) => (
+            <QAItem
+              key={genRandomKey()}
+              content={item}
+              index={listType === LIST_TYPE.NUM ? `${index + 1}` : null}
+            />
+          ))
         }
       </div>
     </div>
