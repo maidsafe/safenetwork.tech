06338e96f6c9ff0d1b9afe83ce176ae44e6039f5
Southside <293741+willief@users.noreply.github.com>
2022-12-19 19:33:02 +0000

    Improve readability
    Minor change for better grammar        "you" for "your"

--- src/pages/NotFound/content.js
+++ src/pages/NotFound/content.js
@@ -1,4 +1,4 @@
 export default {
   title: '404. Yikes.',
-  desc: 'Looks like we’ve hit a snag, and the page your are looking for is not to be found; not here at least. If you have the time, it would be appreciated if you could [report it on the forum](https://safenetforum.org).'
+  desc: 'Looks like we’ve hit a snag, and the page you are looking for is not to be found; not here at least. If you have the time, it would be appreciated if you could [report it on the forum](https://safenetforum.org).'
 }
